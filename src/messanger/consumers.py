4e3f47d6dd9cf23a57f34a42c0821e22de6b80a4
Anirudha Jadhav <anirudhasj441@gmail.com>
2023-10-18 20:33:09 +0530

    dev: add websocket consumer

--- src/messanger/consumers.py
+++ src/messanger/consumers.py
@@ -1,36 +1,20 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-# class ChatConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.room_name = "test_consumer"
-#         self.room_group_name = "test_consumer_group"
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.room_name,
-#             self.room_group_name
-#         )
-#         self.accept()
-#         self.send(text_data=json.dumps({'status': 'connected'}))
-
-#     def receive(self, text_data=None, bytes_data=None):
-#         pass
-
-#     def disconnect(self, code):
-#         pass
-
-
+ 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = "test_consumer"
-        self.room_group_name = "test_consumer_group"
+        # self.room_group_name = "test_consumer_group"
 
         await self.channel_layer.group_add(
             self.room_name, 
-            self.room_group_name
+            self.channel_name
         )
 
         await self.accept()
         await self.send(text_data=json.dumps({'status': 'connected'}))
+
+
 
     async def receive(self, text_data):
         print(text_data)
@@ -40,6 +24,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_name,
-            self.room_group_name
+            self.channel_name
         )
         await self.send(text_data=json.dumps({'message': 'Bye!'}))
+
+    async def sendCounter(self, event):
+        print("Called send Counter")
+        await self.send(
+            text_data=json.dumps(
+                {
+                    'count': json.dumps(event['values'])
+                }
+            )
+        )
--- src/messanger/views.py
+++ src/messanger/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+import time
 
 # Create your views here.
 
-@api_view()
-def index(request):
-    return Response({'status': 200, 'message': "Hello World!"})
+class index(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        channel_layer = get_channel_layer()
+        print(channel_layer)
+        print("hello")
+        for i in range(10):
+            async_to_sync(channel_layer.group_send)(
+                'test_consumer', {
+                    'type': 'sendCounter',
+                    'values': i
+                }
+            )
+            time.sleep(1)
+        return Response({'status': 200, 'message': "Hello World!"})
